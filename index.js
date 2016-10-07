623408586bc413d869549a691b81c77b96ff879c
Guilherme Viana <ggviana@hotmail.com.br>
2016-10-06 22:47:52 -0300

    Converting tabs to spaces

--- index.js
+++ index.js
@@ -1,189 +1,189 @@
 ;(function (root) {
 
-	var _ = {
+  var _ = {
 
-		util: {
+    util: {
 
-			asc  (a, b) { return a > b },
-			desc (a, b) { return a < b },
-			truthy (value) { return value === true },
-			firstOr (arr, value) { return arr.length ? arr[0] : value },
-			inArray (arr, value) { return Boolean(~arr.indexOf(value)) }
+      asc  (a, b) { return a > b },
+      desc (a, b) { return a < b },
+      truthy (value) { return value === true },
+      firstOr (arr, value) { return arr.length ? arr[0] : value },
+      inArray (arr, value) { return Boolean(~arr.indexOf(value)) }
 
-		},
+    },
 
-		chain (arr) {
-			function chainer (collection) {
-				var self = {
-					get () {
-						return collection
-					}
-				}
+    chain (arr) {
+      function chainer (collection) {
+        var self = {
+          get () {
+            return collection
+          }
+        }
 
-				for( var method in _ ) {
-					if (typeof _[method] === 'function') {
-						(function (oldMethod) {
-							self[method] = function () {
-								var args = _.toArray(arguments)
-								args.unshift(collection)
+        for( var method in _ ) {
+          if (typeof _[method] === 'function') {
+            (function (oldMethod) {
+              self[method] = function () {
+                var args = _.toArray(arguments)
+                args.unshift(collection)
 
-								return chainer(oldMethod.apply(null, args))
-							}
-						})(_[method])
-					} else {
-						self[method] = _[method]
-					}
+                return chainer(oldMethod.apply(null, args))
+              }
+            })(_[method])
+          } else {
+            self[method] = _[method]
+          }
 
-				}
+        }
 
-				return self
-			}
+        return self
+      }
 
-			return chainer(arr)
-		},
+      return chainer(arr)
+    },
 
-		reduce (arr, callback, initalValue) {
-			var args = _.toArray(arguments)
+    reduce (arr, callback, initalValue) {
+      var args = _.toArray(arguments)
 
-			if (args.length === 2)
-				return arr.reduce(callback)
+      if (args.length === 2)
+        return arr.reduce(callback)
 
-			return arr.reduce(callback, initalValue)
-		},
-		
-		each (arr, callback) {
-			return _.reduce(arr, function (collection, value, index, arr) {
-				callback(value, index, arr)
-			}, undefined)
-		},
+      return arr.reduce(callback, initalValue)
+    },
+    
+    each (arr, callback) {
+      return _.reduce(arr, function (collection, value, index, arr) {
+        callback(value, index, arr)
+      }, undefined)
+    },
 
-		map (arr, callback) {
-			return _.reduce(arr, function (collection, value, index, arr) {
-				collection.push(callback(value, index, arr))
+    map (arr, callback) {
+      return _.reduce(arr, function (collection, value, index, arr) {
+        collection.push(callback(value, index, arr))
 
-				return collection
-			}, [])
-		},
+        return collection
+      }, [])
+    },
 
-		filter (arr, callback) {
-			return _.reduce(arr, function (collection, value, index, arr) {
+    filter (arr, callback) {
+      return _.reduce(arr, function (collection, value, index, arr) {
 
-				if (_.util.truthy(callback(value, index, arr)))
-					collection.push(value)
+        if (_.util.truthy(callback(value, index, arr)))
+          collection.push(value)
 
-				return collection
-			}, [])
-		},
+        return collection
+      }, [])
+    },
 
-		some (arr, callback) {
-			return _.chain(arr)
-				.map(callback)
-				.filter(_.util.truthy)
-				.get()
-				.length > 0
-		},
+    some (arr, callback) {
+      return _.chain(arr)
+        .map(callback)
+        .filter(_.util.truthy)
+        .get()
+        .length > 0
+    },
 
-		every (arr, callback) {
-			return _.chain(arr)
-				.map(callback)
-				.filter(_.util.truthy)
-				.get()
-				.length === arr.length
-		},
+    every (arr, callback) {
+      return _.chain(arr)
+        .map(callback)
+        .filter(_.util.truthy)
+        .get()
+        .length === arr.length
+    },
 
-		flatten (arr) {
-			return _.reduce(arr, function (collection, value) {
-				return collection.concat(value)
-			}, [])
-		},
+    flatten (arr) {
+      return _.reduce(arr, function (collection, value) {
+        return collection.concat(value)
+      }, [])
+    },
 
-		unique (arr) {
-			return _.reduce(arr, function (collection, value) {
+    unique (arr) {
+      return _.reduce(arr, function (collection, value) {
 
-				if (!_.util.inArray(collection, value))
-					collection.push(value)
+        if (!_.util.inArray(collection, value))
+          collection.push(value)
 
-				return collection
-			}, [])
-		},
+        return collection
+      }, [])
+    },
 
-		toArray (arrlike) {
-			return Array.prototype.slice.call(arrlike)
-		},
+    toArray (arrlike) {
+      return Array.prototype.slice.call(arrlike)
+    },
 
-		groupBy (arr, key) {
-			return _.chain(arr)
-				.extract(key)
-				.unique()
-				.map(function (group) {
-					return _.filter(arr, function (item) {
-						return item[key] === group
-					})
-				})
-				.get()
-		},
+    groupBy (arr, key) {
+      return _.chain(arr)
+        .extract(key)
+        .unique()
+        .map(function (group) {
+          return _.filter(arr, function (item) {
+            return item[key] === group
+          })
+        })
+        .get()
+    },
 
-		indexes (arr) {
-			return _.map(arr, function (_, index) { 
-				return index 
-			})
-		},
+    indexes (arr) {
+      return _.map(arr, function (_, index) { 
+        return index 
+      })
+    },
 
-		extract (arr, key) {
-			return _.map(arr, function (item) {
-				return item[key]
-			})
-		},
+    extract (arr, key) {
+      return _.map(arr, function (item) {
+        return item[key]
+      })
+    },
 
-		head (arr) {
-			return _.filter(arr, function (_, i) {
-				return i < 0
-			})
-		},
+    head (arr) {
+      return _.filter(arr, function (_, i) {
+        return i < 0
+      })
+    },
 
-		tail (arr) {
-			return _.filter(arr, function (_, i) {
-				return i > 0
-			})
-		},
+    tail (arr) {
+      return _.filter(arr, function (_, i) {
+        return i > 0
+      })
+    },
 
-		fill (arr, target) {
-			return _.map(arr, function (value, index, arr) {
-				if (typeof target === 'function') return target(value, index, arr)
-				return target
-			})
-		},
+    fill (arr, target) {
+      return _.map(arr, function (value, index, arr) {
+        if (typeof target === 'function') return target(value, index, arr)
+        return target
+      })
+    },
 
-		reverse (arr) {
-			return _.chain(arr)
-				.indexes()
-				.sort(_.util.desc)
-				.map(function (index) {
-					return arr[index]
-				})
-				.get()
-		},
+    reverse (arr) {
+      return _.chain(arr)
+        .indexes()
+        .sort(_.util.desc)
+        .map(function (index) {
+          return arr[index]
+        })
+        .get()
+    },
 
-		sort (arr, callback) {
-			return arr.sort(callback)
-		},
+    sort (arr, callback) {
+      return arr.sort(callback)
+    },
 
-		max (arr) {
-			return _.reduce(arr, function (current, value) {
-				if (current < value) return value
-				return current
-			}, _.util.firstOr(arr, -Infinity))
-		},
+    max (arr) {
+      return _.reduce(arr, function (current, value) {
+        if (current < value) return value
+        return current
+      }, _.util.firstOr(arr, -Infinity))
+    },
 
-		min (arr) {
-			return _.reduce(arr, function (current, value) {
-				if (current > value) return value
-				return current
-			}, _.util.firstOr(arr, Infinity))
-		},
+    min (arr) {
+      return _.reduce(arr, function (current, value) {
+        if (current > value) return value
+        return current
+      }, _.util.firstOr(arr, Infinity))
+    },
 
-	}
+  }
 
-	root._ = root.reduceIt = _
+  root._ = root.reduceIt = _
 
 }(this));
